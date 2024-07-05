f61172b3bdb45d562d88870217c6f340d57bf781
kartik-gour <kartikgour908@gmail.com>
2024-07-05 07:31:57 -0400

    first implementation of singleton design pattern
    Signed-off-by: kartik-gour <kartikgour908@gmail.com>

---
+++ src/first_implementation/Database.java
@@ -0,0 +1,15 @@
+package first_implementation;
+
+public class Database {
+
+    private static Database instance = null;
+    private Database() {}
+
+    public static Database getInstance(){
+        if (instance == null)
+        {
+            instance = new Database();
+        }
+        return instance;
+    }
+}
